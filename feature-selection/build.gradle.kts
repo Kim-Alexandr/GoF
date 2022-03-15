8d967718f02914fd3c5251319375116f7e13e823
b4sel <b4sel.work@gmail.com>
2022-03-15 13:34:44 +0600

    избавился от лишних implementation, добавил product-solid (wip) пока не получилось его использовать

--- feature-selection/build.gradle.kts
+++ feature-selection/build.gradle.kts
@@ -6,9 +6,6 @@ dependencies {
     implementation(project(":shared-navigation"))
     implementation(project(":shared-core"))
 
-    //todo вот это обяз надо убрать
-    implementation(project(":feature-solid"))
-
     implementation(AndroidX.fragment)
     implementation(AndroidX.material)
     implementation(Libraries.viewBinding)
